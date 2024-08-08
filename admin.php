63afc1f291316a1b63499dc68bcd2fa084922621
erwinsantander <santandererwin4@gmail.com>
2024-08-07 22:25:17 -0700

    commit

--- admin.php
+++ admin.php
@@ -1,84 +1,81 @@
 <?php
-$page_title = 'Admin Home Page';
-require_once('includes/load.php');
-// Check user permissions
-page_require_level(1);
-
-// Retrieve counts
-$c_categorie = count_by_id('categories');
-$c_product = count_by_id('products');
-$c_sale = count_by_id('sales');
-$c_user = count_by_id('users');
-
-// Fetch highest selling products
-$products_sold = find_higest_saleing_product('10');
-// Fetch recent products
-$recent_products = find_recent_product_added('5');
-// Fetch recent sales
-$recent_sales = find_recent_sale_added('5');
+  $page_title = 'Admin Home Page';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(1);
+?>
+<?php
+ $c_categorie     = count_by_id('categories');
+ $c_product       = count_by_id('products');
+ $c_sale          = count_by_id('sales');
+ $c_user          = count_by_id('users');
+ $products_sold   = find_higest_saleing_product('10');
+ $recent_products = find_recent_product_added('5');
+ $recent_sales    = find_recent_sale_added('5')
 ?>
 <?php include_once('layouts/header.php'); ?>
 <div class="row">
    <div class="col-md-6">
    </div>
 </div>
-<div class="row">
+  <div class="row">
     <a href="users.php" style="color:black;">
-        <div class="col-md-3">
-           <div class="panel clearfix">
-             <div class="panel-icon pull-left bg-secondary1">
-              <i class="glyphicon glyphicon-user"></i>
-            </div>
-            <div class="panel-value pull-right">
-              <h2 class="margin-top">&nbsp; <?php echo $c_user['total']; ?> </h2>
-              <p class="text-muted"> &nbsp;Users</p>
-            </div>
-           </div>
+		<div class="col-md-3">
+       <div class="panel  clearfix">
+         <div class="panel-icon pull-left bg-secondary1">
+          <i class="glyphicon glyphicon-user"></i>
         </div>
-    </a>
-    <a href="categorie.php" style="color:black;">
-        <div class="col-md-3">
-           <div class="panel clearfix">
-             <div class="panel-icon pull-left bg-red">
-              <i class="glyphicon glyphicon-th-large"></i>
-            </div>
-            <div class="panel-value pull-right">
-              <h2 class="margin-top">&nbsp; <?php echo $c_categorie['total']; ?> </h2>
-              <p class="text-muted"> &nbsp;Categories</p>
-            </div>
-           </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top">&nbsp; <?php  echo $c_user['total']; ?> </h2>
+          <p class="text-muted"> &nbsp;Users</p>
         </div>
-    </a>
-    
-    <a href="product.php" style="color:black;">
-        <div class="col-md-3">
-           <div class="panel clearfix">
-             <div class="panel-icon pull-left bg-blue2">
-              <i class="glyphicon glyphicon-shopping-cart"></i>
-            </div>
-            <div class="panel-value pull-right">
-              <h2 class="margin-top">&nbsp; <?php echo $c_product['total']; ?> </h2>
-              <p class="text-muted"> &nbsp;Products</p>
-            </div>
-           </div>
+       </div>
+    </div>
+	</a>
+	<a href="categorie.php" style="color:black;">
+    <div class="col-md-3">
+       <div class="panel  clearfix">
+         <div class="panel-icon pull-left bg-red">
+          <i class="glyphicon glyphicon-th-large"></i>
         </div>
-    </a>
-
-    <a href="sales.php" style="color:black;">
-        <div class="col-md-3">
-           <div class="panel clearfix">
-             <div class="panel-icon pull-left bg-green">
-              <i class="fas fa-money-bill-alt"></i>
-            </div>
-            <div class="panel-value pull-right">
-              <h2 class="margin-top">&nbsp; <?php echo $c_sale['total']; ?></h2>
-              <p class="text-muted"> &nbsp;Sales</p>
-            </div>
-           </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top">&nbsp; <?php  echo $c_categorie['total']; ?> </h2>
+          <p class="text-muted"> &nbsp;Categories</p>
         </div>
-    </a>
+       </div>
+    </div>
+	</a>
+	
+	<a href="product.php" style="color:black;">
+    <div class="col-md-3">
+       <div class="panel  clearfix">
+         <div class="panel-icon pull-left bg-blue2">
+          <i class="glyphicon glyphicon-shopping-cart"></i>
+        </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top">&nbsp; <?php  echo $c_product['total']; ?> </h2>
+          <p class="text-muted"> &nbsp;Products</p>
+        </div>
+       </div>
+    </div>
+	</a>
+  
+	<a href="sales.php" style="color:black;">
+    <div class="col-md-3">
+       <div class="panel clearfix">
+         <div class="panel-icon pull-left bg-green">
+          <i class="fas fa-money-bill-alt"></i>
+        </div>
+        <div class="panel-value pull-right">
+          <h2 class="margin-top">&nbsp; <?php echo $c_sale['total']; ?></h2>
+          <p class="text-muted"> &nbsp;Sales</p>
+        </div>
+       </div>
+    </div>
+</a>
 </div>
 
+  <!-- Middle -->
 <!-- Monthly Sales Chart -->
 <div class="row">
   <div class="col-md-12">
@@ -165,8 +162,9 @@ $recent_sales = find_recent_sale_added('5');
   });
 </script>
 
-<!-- Bottom Panels -->
-<div class="row">
+
+  <!-- Bottom -->
+  <div class="row">
    <div class="col-md-4">
      <div class="panel panel-default">
        <div class="panel-heading">
@@ -182,17 +180,17 @@ $recent_sales = find_recent_sale_added('5');
              <th>Title</th>
              <th>Total Sold</th>
              <th>Total Quantity</th>
-           </tr>
+           <tr>
           </thead>
           <tbody>
-            <?php foreach ($products_sold as $product_sold): ?>
+            <?php foreach ($products_sold as  $product_sold): ?>
               <tr>
                 <td><?php echo remove_junk(first_character($product_sold['name'])); ?></td>
                 <td><?php echo (int)$product_sold['totalSold']; ?></td>
                 <td><?php echo (int)$product_sold['totalQty']; ?></td>
               </tr>
             <?php endforeach; ?>
-          </tbody>
+          <tbody>
          </table>
        </div>
      </div>
@@ -216,7 +214,7 @@ $recent_sales = find_recent_sale_added('5');
          </tr>
        </thead>
        <tbody>
-         <?php foreach ($recent_sales as $recent_sale): ?>
+         <?php foreach ($recent_sales as  $recent_sale): ?>
          <tr>
            <td class="text-center"><?php echo count_id();?></td>
            <td>
@@ -227,6 +225,7 @@ $recent_sales = find_recent_sale_added('5');
            <td><?php echo remove_junk(ucfirst($recent_sale['date'])); ?></td>
            <td>₱<?php echo remove_junk(first_character($recent_sale['price'])); ?></td>
         </tr>
+
        <?php endforeach; ?>
        </tbody>
      </table>
@@ -242,16 +241,17 @@ $recent_sales = find_recent_sale_added('5');
         </strong>
       </div>
       <div class="panel-body">
+
         <div class="list-group">
-          <?php foreach ($recent_products as $recent_product): ?>
-            <a class="list-group-item clearfix" href="edit_product.php?id=<?php echo (int)$recent_product['id'];?>">
+      <?php foreach ($recent_products as  $recent_product): ?>
+            <a class="list-group-item clearfix" href="edit_product.php?id=<?php echo    (int)$recent_product['id'];?>">
                 <h4 class="list-group-item-heading">
                  <?php if($recent_product['media_id'] === '0'): ?>
                     <img class="img-avatar img-circle" src="uploads/products/no_image.png" alt="">
                   <?php else: ?>
-                    <img class="img-avatar img-circle" src="uploads/products/<?php echo $recent_product['image'];?>" alt="" />
-                  <?php endif;?>
-                  <?php echo remove_junk(first_character($recent_product['name']));?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $recent_product['image'];?>" alt="" />
+                <?php endif;?>
+                <?php echo remove_junk(first_character($recent_product['name']));?>
                   <span class="label label-warning pull-right">
                   ₱<?php echo (int)$recent_product['sale_price']; ?>
                   </span>
@@ -266,8 +266,10 @@ $recent_sales = find_recent_sale_added('5');
  </div>
 </div>
  </div>
+  <div class="row">
 
-<?php if ($msg): ?>
+  </div>
+  <?php if ($msg): ?>
 <script>
     Swal.fire({
         icon: '<?php echo $msg['type']; ?>',
@@ -277,5 +279,6 @@ $recent_sales = find_recent_sale_added('5');
     });
 </script>
 <?php endif; ?>
+
 
 <?php include_once('layouts/footer.php'); ?>
