7a68bc3602b9bf5b3bc124252464026684daa87e
erwinsantander <santandererwin4@gmail.com>
2024-08-08 01:34:11 -0700

    commit

--- add_sale.php
+++ add_sale.php
@@ -1,12 +1,15 @@
 <?php
 $page_title = 'Add Sale';
 require_once('includes/load.php');
+// Check user permission
 page_require_level(3);
 
+// Initialize variables
 $msg = [];
-$errors = [];
+$errors = []; // Initialize errors array for validation
 $products = [];
 
+// Handle the sale addition
 if (isset($_POST['add_sale'])) {
     $req_fields = array('s_id', 'quantity', 'price', 'total', 'date');
     validate_fields($req_fields);
@@ -34,6 +37,7 @@ if (isset($_POST['add_sale'])) {
     }
 }
 
+// Fetch sales for display with product names
 $sales_query = "
     SELECT sales.id, sales.product_id, sales.price, sales.qty, sales.date, name AS product_name
     FROM sales
@@ -41,6 +45,7 @@ $sales_query = "
 ";
 $sales_result = $db->query($sales_query);
 
+// Fetch products for the drop-down menu
 $products_query = "SELECT id, name FROM products";
 $products_result = $db->query($products_query);
 
@@ -49,6 +54,7 @@ include_once('layouts/header.php');
 
 <div class="row">
   <div class="col-md-6">
+    <!-- Sale addition form -->
     <form method="post" action="add_sale.php">
         <div class="form-group">
             <label for="s_id">Product:</label>
@@ -67,11 +73,11 @@ include_once('layouts/header.php');
         </div>
         <div class="form-group">
             <label for="price">Price:</label>
-            <input type="text" class="form-control" name="price" id="price" readonly>
+            <input type="text" class="form-control" name="price" id="price" required>
         </div>
         <div class="form-group">
             <label for="total">Total:</label>
-            <input type="text" class="form-control" name="total" id="total" readonly>
+            <input type="text" class="form-control" name="total" id="total" required>
         </div>
         <div class="form-group">
             <label for="date">Date:</label>
@@ -82,39 +88,47 @@ include_once('layouts/header.php');
   </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-$(document).ready(function() {
-    $('#s_id').change(function() {
-        var productId = $(this).val();
-        if (productId) {
-            $.ajax({
-                url: 'get_product_price.php',
-                type: 'GET',
-                data: {product_id: productId},
-                dataType: 'json',
-                success: function(data) {
-                    if (data.price) {
-                        $('#price').val(data.price);
-                        calculateTotal();
-                    }
-                }
-            });
-        }
-    });
-
-    $('#quantity').on('input', function() {
-        calculateTotal();
-    });
-
-    function calculateTotal() {
-        var price = parseFloat($('#price').val()) || 0;
-        var quantity = parseInt($('#quantity').val()) || 0;
-        var total = price * quantity;
-        $('#total').val(total.toFixed(2));
-    }
-});
-</script>
+<div class="row">
+  <div class="col-md-12">
+    <div class="panel panel-default">
+      <div class="panel-heading clearfix">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>Sale List</span>
+        </strong>
+      </div>
+      <div class="panel-body">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Qty</th>
+              <th>Total</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody id="product_info">
+            <?php while ($sale = $sales_result->fetch_assoc()): ?>
+              <tr>
+                <td><?php echo htmlspecialchars($sale['product_name']); ?></td>
+                <td><?php echo htmlspecialchars($sale['price']); ?></td>
+                <td><?php echo htmlspecialchars($sale['qty']); ?></td>
+                <td><?php echo htmlspecialchars($sale['price'] * $sale['qty']); ?></td>
+                <td><?php echo htmlspecialchars($sale['date']); ?></td>
+                <td>
+                    <a href="edit_sale.php?id=<?php echo htmlspecialchars($sale['id']); ?>" class="btn btn-warning">Edit</a>
+                    <a href="delete_sale.php?id=<?php echo htmlspecialchars($sale['id']); ?>" class="btn btn-warning" style="background-color:grey">Delete</a>
+                </td>
+              </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php if (isset($msg)): ?>
 <script>
