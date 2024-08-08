807d359719f1cd4217b40efda6e0ec5e4d34afe9
erwinsantander <santandererwin4@gmail.com>
2024-08-08 03:40:47 -0700

    commit

--- edit_sale.php
+++ edit_sale.php
@@ -2,83 +2,90 @@
   $page_title = 'Edit sale';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
-   page_require_level(3);
+  page_require_level(3);
 ?>
+
 <?php
-$sale = find_by_id('sales',(int)$_GET['id']);
-if(!$sale){
-  $session->msg("d","Missing product id.");
+$sale = find_by_id('sales', (int)$_GET['id']);
+if (!$sale) {
+  $session->msg("d", "Missing product id.");
   redirect('sales.php');
 }
 ?>
-<?php $product = find_by_id('products',$sale['product_id']); ?>
+<?php $product = find_by_id('products', $sale['product_id']); ?>
+
 <?php
-
-  if(isset($_POST['update_sale'])){
-    $req_fields = array('title','quantity','price','total', 'date' );
+if (isset($_POST['update_sale'])) {
+    $req_fields = array('title', 'quantity', 'price', 'total', 'date');
     validate_fields($req_fields);
-        if(empty($errors)){
-          $p_id      = $db->escape((int)$product['id']);
-          $s_qty     = $db->escape((int)$_POST['quantity']);
-          $s_total   = $db->escape($_POST['total']);
-          $date      = $db->escape($_POST['date']);
-          $s_date    = date("Y-m-d", strtotime($date));
-
-          $sql  = "UPDATE sales SET";
-          $sql .= " product_id= '{$p_id}',qty={$s_qty},price='{$s_total}',date='{$s_date}'";
-          $sql .= " WHERE id ='{$sale['id']}'";
-          $result = $db->query($sql);
-          if( $result && $db->affected_rows() === 1){
-                    update_product_qty($s_qty,$p_id);
-                    $session->msg('s',"Sale updated.");
-                    redirect('edit_sale.php?id='.$sale['id'], false);
-                  } else {
-                    $session->msg('d',' Sorry failed to updated!');
-                    redirect('sales.php', false);
-                  }
-        } else {
-           $session->msg("d", $errors);
-           redirect('edit_sale.php?id='.(int)$sale['id'],false);
+    if (empty($errors)) {
+        $p_id = $db->escape((int)$product['id']);
+        $s_qty = $db->escape((int)$_POST['quantity']);
+        $s_total = $db->escape($_POST['total']);
+        $date = $db->escape($_POST['date']);
+        $s_date = date("Y-m-d", strtotime($date));
+        
+        // Check stock availability
+        $available_qty = (int)$product['quantity'];
+        if ($s_qty > $available_qty) {
+            $session->msg("d", "Quantity exceeds available stock!");
+            redirect('edit_sale.php?id=' . (int)$sale['id'], false);
         }
-  }
 
+        // Update sale record
+        $sql = "UPDATE sales SET";
+        $sql .= " product_id= '{$p_id}', qty={$s_qty}, price='{$s_total}', date='{$s_date}'";
+        $sql .= " WHERE id ='{$sale['id']}'";
+        $result = $db->query($sql);
+        
+        if ($result && $db->affected_rows() === 1) {
+            update_product_qty($s_qty, $p_id); // Ensure this function handles stock properly
+            $session->msg('s', "Sale updated.");
+            redirect('edit_sale.php?id=' . (int)$sale['id'], false);
+        } else {
+            $session->msg('d', ' Sorry failed to update!');
+            redirect('sales.php', false);
+        }
+    } else {
+        $session->msg("d", $errors);
+        redirect('edit_sale.php?id=' . (int)$sale['id'], false);
+    }
+}
 ?>
 <?php include_once('layouts/header.php'); ?>
 <div class="row">
-
 </div>
 <div class="row">
-
   <div class="col-md-12">
-  <div class="panel">
-    <div class="panel-heading clearfix">
-      <strong>
-        <span class="glyphicon glyphicon-th"></span>
-        <span>All Sales</span>
-     </strong>
-     <div class="pull-right">
-       <a href="sales.php" class="btn btn-primary">Show all sales</a>
-     </div>
-    </div>
-    <div class="panel-body">
-       <table class="table table-bordered">
-         <thead>
-          <th> Product title </th>
-          <th> Qty </th>
-          <th> Price </th>
-          <th> Total </th>
-          <th> Date</th>
-          <th> Action</th>
-         </thead>
-           <tbody  id="product_info">
-              <tr>
+    <div class="panel">
+      <div class="panel-heading clearfix">
+        <strong>
+          <span class="glyphicon glyphicon-th"></span>
+          <span>All Sales</span>
+        </strong>
+        <div class="pull-right">
+          <a href="sales.php" class="btn btn-primary">Show all sales</a>
+        </div>
+      </div>
+      <div class="panel-body">
+        <table class="table table-bordered">
+          <thead>
+            <th> Product title </th>
+            <th> Qty </th>
+            <th> Price </th>
+            <th> Total </th>
+            <th> Date</th>
+            <th> Action</th>
+          </thead>
+          <tbody id="product_info">
+            <tr>
               <form method="post" action="edit_sale.php?id=<?php echo (int)$sale['id']; ?>">
                 <td id="s_name">
                   <input type="text" class="form-control" id="sug_input" name="title" value="<?php echo remove_junk($product['name']); ?>">
                   <div id="result" class="list-group"></div>
                 </td>
                 <td id="s_qty">
-                  <input type="text" class="form-control" name="quantity" value="<?php echo (int)$sale['qty']; ?>">
+                  <input type="number" class="form-control" name="quantity" value="<?php echo (int)$sale['qty']; ?>" min="0">
                 </td>
                 <td id="s_price">
                   <input type="text" class="form-control" name="price" value="<?php echo remove_junk($product['sale_price']); ?>" >
@@ -87,21 +94,20 @@ if(!$sale){
                   <input type="text" class="form-control" name="total" value="<?php echo remove_junk($sale['price']); ?>">
                 </td>
                 <td id="s_date">
-                  <input type="date" class="form-control datepicker" name="date" data-date-format="" value="<?php echo remove_junk($sale['date']); ?>">
+                  <input type="date" class="form-control datepicker" name="date" value="<?php echo remove_junk($sale['date']); ?>">
                 </td>
                 <td>
                   <button type="submit" name="update_sale" class="btn btn-primary">Update sale</button>
                 </td>
               </form>
-              </tr>
-           </tbody>
-       </table>
-
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
-  </div>
-
 </div>
+
 <?php if ($msg): ?>
 <script>
     Swal.fire({
@@ -112,4 +118,5 @@ if(!$sale){
     });
 </script>
 <?php endif; ?>
+
 <?php include_once('layouts/footer.php'); ?>
