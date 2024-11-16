b979adabdc809c16dbbd503fd8f1ed7ea0590733
erwinsantander <santandererwin4@gmail.com>
2024-11-16 15:59:36 -0800

    first commit

---
+++ includes/config.php
@@ -0,0 +1,32 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+define( 'DB_HOST', '127.0.0.1' );          // Set database host
+<<<<<<< HEAD
+<<<<<<< HEAD
+define( 'DB_USER', 'root' );             // Set database user
+define( 'DB_PASS', '' );             // Set database password
+define( 'DB_NAME', 'inventory_system' );        // Set database name
+=======
+define( 'DB_USER', 'u510162695_ancminimart' );             // Set database user
+define( 'DB_PASS', '1Ancminimart' );             // Set database password
+define( 'DB_NAME', 'u510162695_ancminimart' );        // Set database name
+>>>>>>> a51023ef536b2ff5ef85b9f67abc0540506da783
+=======
+define( 'DB_USER', 'u510162695_ancminimart' );             // Set database user
+define( 'DB_PASS', '1Ancminimart' );             // Set database password
+define( 'DB_NAME', 'u510162695_ancminimart' );        // Set database name
+>>>>>>> a51023ef536b2ff5ef85b9f67abc0540506da783
+
+?>
